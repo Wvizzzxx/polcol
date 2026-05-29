01f77bdea6538df71b7428a6239583378cc5c28b
sojanta <elya228.com@gmail.com>
2026-05-29 23:12:36 +0300

    feat: add dark theme with toggle switch in header

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
+import { ThemeProvider } from './context/ThemeContext'
 import './index.css'
 import App from './App.jsx'
 
@@ -12,8 +13,10 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/polcol">
       <QueryClientProvider client={queryClient}>
-        <App />
-        <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
+        <ThemeProvider>
+          <App />
+          <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
+        </ThemeProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
